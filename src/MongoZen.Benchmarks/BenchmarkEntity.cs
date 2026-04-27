c8fba492bfacc28ea84c009ab00003caf952b3de
Michael Yarichuk <michael.yarichuk@gmail.com>
2026-04-27 08:34:25 +0300

    docs: move deep dive to GitHub wiki, update performance benchmarks with polymorphism and partial update comparisons

--- src/MongoZen.Benchmarks/BenchmarkEntity.cs
+++ src/MongoZen.Benchmarks/BenchmarkEntity.cs
@@ -2,6 +2,22 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace MongoZen.Benchmarks;
 
+[BsonKnownTypes(typeof(BaseData), typeof(DerivedDataA), typeof(DerivedDataB))]
+public abstract class BaseData
+{
+    public string Type { get; set; } = null!;
+}
+
+public class DerivedDataA : BaseData
+{
+    public string InfoA { get; set; } = null!;
+}
+
+public class DerivedDataB : BaseData
+{
+    public int InfoB { get; set; }
+}
+
 public class BenchmarkEntity
 {
     [BsonId]
@@ -18,6 +34,8 @@ public class BenchmarkEntity
     public List<string> Tags { get; set; } = [];
 
     public Dictionary<string, string> Metadata { get; set; } = [];
+
+    public BaseData? PolymorphicData { get; set; }
 
     public int Version { get; set; }
 }
